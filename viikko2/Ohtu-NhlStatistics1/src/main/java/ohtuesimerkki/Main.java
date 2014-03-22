2eb2818023b000e7da0a5f693f64b913c77bad07
Ville Virtanen <ville@mevirtaset.com>
2014-03-23 01:44:13 +0200

    muutos

--- viikko2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
+++ viikko2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
@@ -3,9 +3,9 @@ package ohtuesimerkki;
 public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics( new PlayerReader("http://nhlstats-2013-14.herokuapp.com/players.txt") );
+        
 
-
-       /* System.out.println("Philadelphia Flyers");
+        System.out.println("Philadelphia Flyers");
        for (Player player : stats.team("PHI") ) {
            System.out.println( player );
         }
@@ -15,6 +15,6 @@ public class Main {
         System.out.println("Top scorers");
         for (Player player : stats.topScorers(10) ) {
             System.out.println( player );
-        }  */      
+        }       
     }
 }
